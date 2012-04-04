60c02b5fd51a259779df49add4b2fdf293d95c52
WanWizard <wanwizard@wanwizard.eu>
2012-04-04 22:04:53 +0200

    more layout fixes

--- fuel/themes/default/api/intro.php
+++ fuel/themes/default/api/intro.php
@@ -1,11 +1,5 @@
-<div>
-	<h1>Class API Documentation</h1>
-</div>
-<div class="clearfix"></div>
-<div id="api">
-	<p>&nbsp;</p>
-	<p>Welcome to the FuelPHP API documentation.</p>
-	<p>Use the navigation menu on the left to search for functions or classes.</p>
-	<p>Click on the package name to reveal the list of functions or classes in that package, and click on one to view the API documentation.</p>
-	<p>When viewing the documentation, the navigation menu will highlight all functions and classes defined in the file you have selected.</p>
-</div>
+<h3>The FuelPHP API</h3>
+<p>Use the dropdown box to select the version of FuelPHP you are using.</p>
+<p>Use the navigation menu on the left to search for functions or classes.</p>
+<p>Click on the package name to reveal the list of functions or classes in that package, and click on one to view the API documentation.</p>
+<p>When viewing the documentation, the navigation menu will highlight all functions and classes defined in the file you have selected.</p>
