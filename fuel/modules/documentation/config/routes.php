83a39001eb439afb0c9f49060cf498cd10474370
WanWizard <wanwizard@wanwizard.eu>
2012-06-16 12:31:04 +0200

    fixed a route, some cosmetic updates

--- fuel/modules/documentation/config/routes.php
+++ fuel/modules/documentation/config/routes.php
@@ -1,4 +1,5 @@
 <?php
 return array(
-	'documentation/(:segment)/(:num)(:any)' => 'documentation/$1/index/$2$3',
+	'documentation/(:segment)/(:num)' => 'documentation/$1/index/$2',
+	'documentation/(:segment)/(:num)/(:any)' => 'documentation/$1/index/$2/$3',
 );
--- fuel/themes/default/documentation/notfound.php
+++ fuel/themes/default/documentation/notfound.php
@@ -6,6 +6,10 @@
 	</h6>
 	<p>&nbsp;</p>
 	<?php if (\Auth::has_access('access.staff') or \Session::get('ninjauth.authentication.provider', false) == 'github'): ?>
-		<h5>Click on the "New page" button to add a new page to the documenation.</h5>
+		<?php if ($page): ?>
+			<h5>Click on the "Edit page" button to add content to this topic.</h5>
+	<?php else: ?>
+			<h5>Click on the "New page" button to add a new page to the documenation.</h5>
+	<?php endif; ?>
 	<?php endif; ?>
 </div>
